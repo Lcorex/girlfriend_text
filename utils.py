29a6ce7f22e10b893c1393f32b8b37ded4c68553
Lcorex <88617784+Lcorex@users.noreply.github.com>
2021-08-08 16:07:46 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,3 @@
 phone_number = "phone number"
-message = ["Good morning babe", "Good morning my love", "Good morning cutie"]
-scheduled_time = "08:00"
+message = ["Good morning babe and princess"]
+scheduled_time = "08.00"
